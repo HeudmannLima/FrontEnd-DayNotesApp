cc4f0b0c232e4d88be7c81e2762089225e77c2c1
Heudmann Lima <heudmannlima@MacBook-Air-de-Heudmann.local>
2020-07-23 18:42:10 -0400

    Atualizacao salvar ao editar nota

--- src/components/Notes/index.js
+++ src/components/Notes/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './styles.css';
 import { AiTwotoneDelete, AiOutlineExclamation, AiTwotoneEdit } from 'react-icons/ai';
+import api from '../../services/api';
 
 function Notes({ data, handleChangePriority, handleDelete }) {
+  const [ newNote, setNewNote ] = useState('');
+
+  function handleEdit(el, priority) {
+    el.style.cursor = 'text';
+    el.style.borderRadius = '5px';
+
+    if (priority) {
+      el.style.boxShadow = '0 0 5px white';      
+    } else {
+      el.style.boxShadow = '0 0 5px gray';
+    }
+  }
+
+  async function handleSave(el, notes) {
+    el.style.cursor = 'default';
+    el.style.boxShadow = 'none';
+
+    if (newNote && newNote !== notes) {
+      await api.post(`/contents/${data._id}`, {
+        notes: newNote
+      });
+    }
+  }
 
   return (
     <li className={data.priority ? "notepad-infos-priority" : "notepad-infos"}>
@@ -19,7 +43,12 @@ function Notes({ data, handleChangePriority, handleDelete }) {
         </div> 
       </div>
 
-      <textarea disabled value={data.notes} />
+      <textarea
+        defaultValue={data.notes}
+        onChange={e => setNewNote(e.target.value)}
+        onClick={el => handleEdit(el.target, data.priority)}
+        onBlur={el => handleSave(el.target, data.notes)}
+      />
       <p>
         <AiOutlineExclamation
           onClick={() => handleChangePriority(data._id)}
